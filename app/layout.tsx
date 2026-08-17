42e93ec439e7d2335dfe9ab5d6ac99e9089b4ffe
RaphaelModenez <raphamodenez@gmail.com>
2026-08-17 17:06:36 -0300

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import "./globals.css";
+import "./iphone.css";
 import BottomNav from "@/components/BottomNav";
 
 export const metadata: Metadata = {
